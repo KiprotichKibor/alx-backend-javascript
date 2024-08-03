3722ce8aa58e4bc8d80f95764167be1ae0f86c9c
KiprotichKibor <kiborotich24@gmail.com>
2024-08-03 23:27:24 +0000

    dollar dollar bill yoh!

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -9,7 +9,7 @@ export default class Currency {
   }
 
   set code(value) {
-    if (value !== 'string') {
+    if (typeof value !== 'string') {
       throw new TypeError('code must be a string');
     }
     this._code = value;
@@ -20,7 +20,7 @@ export default class Currency {
   }
 
   set name(value) {
-    if (value !== 'string') {
+    if (typeof value !== 'string') {
       throw new TypeError('name must be a string');
     }
     this._name = value;
